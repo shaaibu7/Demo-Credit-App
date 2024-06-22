47c14c8e4d67c2296df456d09c162a5b9bd6e6ce
shaaibu suleiman <shaaibusuleiman9@gmail.com>
2024-06-22 01:50:05 +0100

    add docker compose config

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, { Application, Request, Response } from "express";
 
 const app: Application = express();
-const port: number = 3000 || process.env.PORT;
+const port: number = 4000 || process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello world');
